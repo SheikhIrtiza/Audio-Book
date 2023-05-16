eb05b52e4cc419d3869f99ea65f35effaea4ab2b
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-05-17 00:05:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ book = open('oop.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
+# using speaker for narration
 speaker = pyttsx3.init()
 for num in range(7, pages):
     page = pdfReader.getPage(7)
